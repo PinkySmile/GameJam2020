bc2988a718606d91031384ef4f08bd8e01374759
Andgel <andgel.halley@epitech.eu>
2020-01-31 20:22:44 +0100

    Added basic CMake + main

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** GameJam2020
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World !" << std::endl;
+    return EXIT_SUCCESS;
+}
